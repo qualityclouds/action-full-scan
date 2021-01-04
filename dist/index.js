72a3f475e186f7a9ab1f51268813286e4faba830
Pau Gonzàlez Albiñana <pgonzalez@qualityclouds.com>
2021-01-04 12:01:53 +0100

    fix: remove params

--- dist/index.js
+++ dist/index.js
@@ -1040,7 +1040,7 @@ async function run() {
 
      
         await exec.exec(`docker pull ${docker_name} -q`);
-        let command = (`docker run --user root -v ${workspace}:/code/:rw --network="host" -e QC_API_KEY=${token} -e ISSUES_THRESHOLD=0 -e TECHDEBT_THRESHOLD=0 -e SEVERITY_THRESHOLD=1 -e diff_mode="1" -t ${docker_name} sf-scan`);
+        let command = (`docker run --user root -v ${workspace}:/code/:rw --network="host" -e QC_API_KEY=${token} -e diff_mode="1" -t ${docker_name} sf-scan`);
 
 
         try {
